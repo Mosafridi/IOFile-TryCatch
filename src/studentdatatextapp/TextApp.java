dee408dbaa07b708c6d85e53aa851aeb89dde80c
Mohammad Shoaib <119535984+Mosafridi@users.noreply.github.com>
2024-11-15 10:50:34 +0000

    first commit

---
+++ src/studentdatatextapp/TextApp.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdatatextapp;
+
+
+public class TextApp {
+
+  
+    public static void main(String[] args) {
+     
+        GUI mygui = new GUI();
+        mygui.setVisible(true);
+        
+     
+    }
+    
+}
